4398e14f0c637afdaedb8bff9b99bfba73de8560
walid <71653653+walid-01@users.noreply.github.com>
2024-10-27 01:41:17 +0100

    deploy

--- src/components/FWhatsapp.jsx
+++ src/components/FWhatsapp.jsx
@@ -1,15 +1,14 @@
 "use client";
 
 import { FloatingWhatsApp } from "react-floating-whatsapp";
-import logoIcon from "../../public/assets/Logo_2.svg";
 
 const FWhatsapp = () => {
   return (
     <div>
       <FloatingWhatsApp
-        phoneNumber="1234567890"
+        phoneNumber="213540075383"
         accountName="Clinic Trip"
-        avatar={logoIcon}
+        // avatar="/assets/Logo_2.png"
         statusMessage="Typically replies instantly"
         chatMessage="Hello, Welcome to CLINIC TRIP. Thank you so much for contacting us. How may I help you??"
         // placeholder="Type your message..."
